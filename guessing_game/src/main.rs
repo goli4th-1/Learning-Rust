1a03c5a8344b5885cb0d906ea85130ac0b1c5c6e
g0li4th <eeeyre@gmail.com>
2024-02-11 00:18:28 -0700

    First 2 programming challenges

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Welcome to the number guessing game");
+    println!("You must guess the number between 1 and 100\n");
+
+    loop {
+        println!("Please guess a number");
+        let mut guess = String::new();
+        io::stdin()
+            .readline(&mut guess)
+            .expect("Failed to read the guessed number");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please only enter integer values\n");
+                continue
+            },
+        }
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Your guess was too low"),
+            Ordering::Greater => println!("Your guess was too high"),
+            Ordering::Equal => {
+                println!("Congradulations!  You guessed correctly!");
+                break;
+            }
+        }
+
+    }
+    
+
+}
